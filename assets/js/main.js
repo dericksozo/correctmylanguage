b3a924dcb776e80609ff2d1a411daf93236141a0
Derick Ruiz <derickiscool@gmail.com>
2016-02-23 13:05:12 +0900

    Styling the textarea in its default state and also making the button smaller.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+(function () {
+
+    /* Some kind of CSS fix to make up for a bug. */
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+      var msViewportStyle = document.createElement('style')
+      msViewportStyle.appendChild(
+        document.createTextNode(
+          '@-ms-viewport{width:auto!important}'
+        )
+      )
+      document.querySelector('head').appendChild(msViewportStyle)
+    }
+
+    autosize($(".js-correctform-textarea"));
+    
+}());
