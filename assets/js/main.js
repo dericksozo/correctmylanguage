d945ec009b421832b1d5af045eb32b931a5de1a1
Derick Ruiz <derickiscool@gmail.com>
2016-02-25 13:09:26 +0900

    The begginings of getting firebase integrated into my page. I have anonymous authentication working and also submitting random data after form submit.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,20 @@
       document.querySelector('head').appendChild(msViewportStyle)
     }
 
+    var myFirebaseRef = new Firebase("https://correctmylanguage.firebaseio.com/");
+    var corrections = myFirebaseRef.child("corrections");
+
+    myFirebaseRef.authAnonymously(function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+        }
+    });
+
     autosize($(".js-correctform-textarea"));
 
     // TODO: Add a check to the form to see if there is at least one character in there and don't show the form unelss there is.
-
-
     // Submitting the form.
     $(".js-correct-form").submit(function (e) {
         e.preventDefault();
@@ -25,6 +34,26 @@
         $submitButton.attr("data-state", "submitting");
         $submitButton.attr("disabled", "disabled");
         $submitButton.attr("value", "提出中...");
+
+        /* Just testing this out with some test data. */
+        myFirebaseRef.set({
+            title: "Hello World!",
+            author: "Firebase",
+            location: {
+                city: "San Francisco",
+                state: "California",
+                zip: 94103
+            }
+        }, function (error) {
+            if (error) {
+                console.log("What was the error?", error);
+            } else {
+                $submitButton.removeAttr("data-state");
+                $submitButton.removeAttr("disabled");
+                $submitButton.attr("value", "提出");
+            }
+        });
+
     });
 
 
