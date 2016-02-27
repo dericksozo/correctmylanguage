dda72b2b65d0f77ab28efb0fd1348a0cb6e97830
Derick Ruiz <derickiscool@gmail.com>
2016-02-27 14:12:23 +0900

    Adding the bug fixing code to the head tag. It just doesn't feel right being inside of main.js. Also, adding some basic Firebase code to test stuff.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,10 @@
 
 (function () {
 
-    /* Some kind of CSS fix to make up for a bug. */
-    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-      var msViewportStyle = document.createElement('style')
-      msViewportStyle.appendChild(
-        document.createTextNode(
-          '@-ms-viewport{width:auto!important}'
-        )
-      )
-      document.querySelector('head').appendChild(msViewportStyle)
-    }
-
     var myFirebaseRef = new Firebase("https://correctmylanguage.firebaseio.com/");
     var corrections = myFirebaseRef.child("corrections");
 
+    // Authenticate the user anonymously
     myFirebaseRef.authAnonymously(function(error, authData) {
         if (error) {
             console.log("Login Failed!", error);
@@ -23,6 +13,7 @@
         }
     });
 
+    // Set up autosize on the textarea.
     autosize($(".js-correctform-textarea"));
 
     // TODO: Add a check to the form to see if there is at least one character in there and don't show the form unelss there is.
@@ -36,23 +27,29 @@
         $submitButton.attr("value", "提出中...");
 
         /* Just testing this out with some test data. */
-        myFirebaseRef.set({
-            title: "Hello World!",
-            author: "Firebase",
-            location: {
-                city: "San Francisco",
-                state: "California",
-                zip: 94103
-            }
-        }, function (error) {
-            if (error) {
-                console.log("What was the error?", error);
-            } else {
-                $submitButton.removeAttr("data-state");
-                $submitButton.removeAttr("disabled");
-                $submitButton.attr("value", "提出");
-            }
-        });
+
+        var authData = myFirebaseRef.getAuth();
+
+        if (authData) {
+
+            /* Save the correction into firebase. */
+            myFirebaseRef.child("corrections").push({
+                correction: $(".js-correctform-textarea").val(),
+                author: authData.uid,
+            }, function (error) {
+                if (error) {
+                    console.log("What was the error?", error);
+                } else {
+                    $submitButton.removeAttr("data-state");
+                    $submitButton.removeAttr("disabled");
+                    $submitButton.attr("value", "提出");
+                }
+            });
+
+        } else {
+            // Need  to do something about this one here.
+            console.log("User is logged out");
+        }
 
     });
 
